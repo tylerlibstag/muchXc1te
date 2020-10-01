8cd95638386ad130cfd3c1e119c3c154a1072708
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-10-01 15:39:55 -0400

    Initial file structure and directions for workflow for Front-End Team

--- aptreact/src/pages/SignUp.js
+++ aptreact/src/pages/SignUp.js
@@ -1,4 +1,18 @@
+import React from "react";
+import "../components/SignUp/SignUp.css";
 
+function SignUp() {
+    return (
+        // Allows user to sign in to existing profile
+        <div>
+            
+        </div>
+        // To be coded:
+        // FORM for user to fill out NAME, EMAIL, desired USERNAME, desired PASSWORD, CONFIRM PASSWORD, and PROFILE PIC
+        
+        // send a call to the users database with all the information they filled out.
+        // Button will send profile to db using POST
+    )
+}
 
-// send a call to the users database with all the information they filled out.
-// Button will send profile to db using POST
+export default SignUp;
