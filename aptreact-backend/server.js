f24035f05096c99d145bc92d256616a3645cea24
BubblyRobot <susyjustice@gmail.com>
2020-09-30 22:58:57 -0400

    removed the file limit

--- aptreact-backend/server.js
+++ aptreact-backend/server.js
@@ -30,9 +30,9 @@ app.use(bodyParser.json());
 
 const multer = Multer({
   storage: Multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
-  },
+  // limits: {
+  //   fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  // },
 });
 
 const bucket = storage.bucket("apt-videos");
