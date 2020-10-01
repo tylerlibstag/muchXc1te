f6453f2c0abf29cf15be811c276f0b0db2a2ff1f
TYTY <tylerlibstag@gmail.com>
2020-09-30 22:03:12 -0400

    added video upload code

--- aptreact-backend/server.js
+++ aptreact-backend/server.js
@@ -1,15 +1,82 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import express from "express";
 import mongoose from "mongoose";
+import Multer from "multer";
 
+
+
+const config = require("./aptreactstorage-52385ce6c45e.json");
+const {format} = require("util");
+const bodyParser = require('body-parser');
+
+const {Storage} = require("@google-cloud/storage");
+
+//instanstiate storage client 
+const storage = new Storage();
+
+const port = process.env.PORT || 9000;
+
+
+//database stuff
 import Data from "./data.js";
 import Videos from "./dbModel.js";
 
 // app config
 const app = express();
-const port = process.env.PORT || 9000;
+
+//multer things 
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
+});
+
+const bucket = storage.bucket("apt-videos");
+
+app.get('/', (req, res) => {
+  res.render('form.pug');
+});
+
+
+
+
 
 // middlewards
+
+app.post('/upload', multer.single('file'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream();
+
+  blobStream.on('error', (err) => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
+});
+
+
 app.use(express.json());
+
+//
 
 //what this is doing is that when we get a request we'll send the headers below.
 app.use((req, res, next) => {
@@ -58,6 +125,8 @@ app.post("/v2/posts", (req, res) => {
     }
   });
 });
+
+
 
 // listen
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
