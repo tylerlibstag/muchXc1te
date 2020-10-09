8fee68ccefa71dc29e3414e2dd1b6c6e10661963
Rocco Losito <roccolosito@gmail.com>
2020-10-08 20:24:57 -0400

    modified lister and loooker js

--- aptreact/src/components/ListerProfile/Lister.js
+++ aptreact/src/components/ListerProfile/Lister.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./ListerProfile.css";
+import Upload from "./Upload";
+import Manage from "./Manage";
 
 function Lister() {
     return (
         // This will show the user the information they input during their signup
-        <div></div>
-
-        // To be coded
-        // A div containing the user's PROFILE PIC, NAME, type of user, and USERNAME (and any other information you see fit - maybe email or option to change password...)
+        // Navbar
+        // A div containing the user's PROFILE PIC, NAME, type of user and USERNAME 
+        // (and any other information you see fit - maybe email or option to change password...)
+        <div>
+            <Upload />
+            <Manage />
+        </div>
     )
 }
 
--- aptreact/src/components/LookerProfile/Looker.js
+++ aptreact/src/components/LookerProfile/Looker.js
@@ -5,9 +5,10 @@ function Looker() {
     return (
         // This will show the user the information they input during their signup
         <div></div>
-
-        // To be coded
-        // A div containing the user's PROFILE PIC, NAME, type of user, and USERNAME (and any other information you see fit - maybe email or option to change password...)
+        // Navbar
+        // A div containing the user's PROFILE PIC, NAME, type of user and USERNAME (and any other information you see fit - maybe email or option to change password...)
+        // div for Videos saved
+        
     )
 }
 
