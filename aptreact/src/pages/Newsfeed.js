6bd8e51c0d32271e3895660a2c219417fa8b4b58
BubblyRobot <susyjustice@gmail.com>
2020-10-10 21:43:48 -0400

    lots of changes

--- aptreact/src/pages/Newsfeed.js
+++ aptreact/src/pages/Newsfeed.js
@@ -15,9 +15,9 @@ function Newsfeed() {
             <FeedNav />
             <Container fluid>
                 <Row>
-                    <Col xs={1}><SideNav /></Col>
-                    <Col xs={10}> <Main /></Col>
-                    <Col xs={1}>Here's the Side Nav.....</Col>
+                    <Col xs={2}><SideNav /></Col>
+                    <Col xs={8}> <Main /></Col>
+                    <Col xs={2}><SideNav /></Col>
 
                 </Row>
             </Container>
