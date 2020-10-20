ad8e0cc132b6a60929ccbb1775aec65ac8aa1d2e
TYTY <tylerlibstag@gmail.com>
2020-10-19 22:38:38 -0400

    work

--- aptreact-backend/routes/auth-routes.js
+++ aptreact-backend/routes/auth-routes.js
@@ -55,15 +55,7 @@ router.get("/register_login", (req, res, next) => {
   })(req, res, next);
 });
 
-router.post(("/addSaved"), (req, res, next) => {
-  console.log('time to save~!!!', req.body)
-  const savedVideos = req.body;
-  LocalUser.update(
-  { $push: { saved_Videos: savedVideos } }
-  ).then(data => res.json(data))
-  (req, res, next);
-  // save to DB!!!!! look  up how to push something to array on LocalUser $push
-});
+
 
 router.get("/users/:id", (req, res) => {
   const id = req.params.id;
--- aptreact/src/components/Newsfeed/VideoSidebar.js
+++ aptreact/src/components/Newsfeed/VideoSidebar.js
@@ -28,7 +28,7 @@ function VideoSidebar({ likes, shares, messages, saved }) {
       <div className="videoSidebar_button">
         {/* <SaveIcon fontSize="large" onClick={} /> */}
         <p>{saved}</p>
-      </div> */}
+      </div> 
 
       <div className="videoSidebar_button">
         <SaveIcon fontSize="large" />
